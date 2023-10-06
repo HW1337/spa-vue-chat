7d8b84416d99ac63fa82ddbfa8e0ac4bcac19823
HW1337 <kskobelin@ya.ru>
2023-10-06 16:40:40 +0700

    modified header and footer

--- web/src/App.vue
+++ web/src/App.vue
@@ -12,7 +12,7 @@ import AppFooter from "./components/layouts/AppFooter.vue"
 import "../public/assets/css/bootstrap.min.css"
 import "../public/assets/font-awesome-4.7.0/css/font-awesome.min.css"
 
-import "../public/assets/js/jquery-3.1.1.js"
+import "../public/assets/js/jquery-3.7.1.min.js"
 import "../public/assets/js/bootstrap.min.js"
 
 export default {
@@ -25,12 +25,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin-top: 0px !important;
 }
 </style>
