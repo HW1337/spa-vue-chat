3b8229b79214c1fc5c28d489cd1760ea433cd0cc
HW1337 <kskobelin@ya.ru>
2023-10-10 04:17:41 +0700

    login feature

--- web/src/main.js
+++ web/src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import RegisterComponent from "./components/RegisterComponent.vue"
+import LoginComponent from "./components/LoginComponent.vue"
 
 const routes = [
     { path: "/register", component: RegisterComponent },
+    { path: "/login", component: LoginComponent }
 ]
 const router = createRouter ({
     history: createWebHistory(),
