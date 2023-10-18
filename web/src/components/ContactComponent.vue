9dfab85649cb1b14d53d1b15f9761de931a13308
HW1337 <kskobelin@ya.ru>
2023-10-19 03:32:57 +0700

    delete contact feature

--- web/src/components/ContactComponent.vue
+++ web/src/components/ContactComponent.vue
@@ -22,7 +22,13 @@
                             <span v-text="contact.name"></span>
                         </td>
     <td v-text="contact.email"></td>
-</tr>
+    <td style="display: flex;">
+        <form v-on:submit.prevent="deleteContact">
+            <input type="hidden" name="email" v-bind:value="contact.email" required />
+            <input type="submit" v-bind:value="isDeleting ? 'Deleting...' : 'Delete'" v-bind:isDeleting="disabled" class="btn btn-danger" />
+    </form>
+</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -41,6 +47,54 @@
         },
  
         methods: {
+            deleteContact: async function () {
+    const self = this
+    const form = event.target
+ 
+    swal.fire({
+        title: 'Вы уверены?',
+        text: "Контакт будет удалён",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Да, удалить!'
+    }).then(async function(result) {
+        if (result.isConfirmed) {
+ 
+            self.isDeleting = true;
+            const formData = new FormData(form);
+ 
+            const response = await axios.post(
+                self.$apiURL + "/contact/delete",
+                formData,
+                {
+                    headers: self.$headers
+                }
+            );
+            self.isDeleting = false;
+ 
+            if (response.data.status == "success") {
+                swal.fire(
+                    'Удалено!',
+                    response.data.message,
+                    'success'
+                );
+ 
+                const contactsArr = self.contacts;
+                for (let a = 0; a < contactsArr.length; a++) {
+                    if (contactsArr[a].email == form.email.value) {
+                        contactsArr.splice(a, 1);
+                        break;
+                    }
+                }
+                self.contacts = contactsArr;
+            } else {
+                swal.fire("Error", response.data.message, "error");
+            }
+        }
+    })
+},
             getData: async function () {
                 const self = this
  
