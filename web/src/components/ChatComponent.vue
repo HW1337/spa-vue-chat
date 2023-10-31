8b94145ea5844bc93c3460a442a7ebe5ab7516b2
HW1337 <kskobelin@ya.ru>
2023-11-01 03:42:43 +0700

    Revert "Merge pull request #56 from HW1337/feature"
    This reverts commit f0e045a9d3ded7e43df5cd63e9e29ef51f0f5979, reversing
    changes made to 3b467b9d728c5f65d631f9a6bc2db838ba8ef760.

--- web/src/components/ChatComponent.vue
+++ web/src/components/ChatComponent.vue
@@ -77,15 +77,6 @@
                 downloadFileName: ""
             }
         },
-
-        watch: {
-            $route: function (to, from) {
-                if (from.href.includes("/chat/")) {
-                    store.commit("setMessages", [])
-                }
-            }
-        },
-
         computed: {
             messages() {
                 return store.getters.getMessages
